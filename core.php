3855f5ee6d916baaa8bb2bc6cdb785b67e49ae6e
Eirbware <eirbware@enseirb-matmeca.fr>
2011-12-15 14:49:43 +0100

    Debug dépendant du dossier actuel

--- core.php
+++ core.php
@@ -6,7 +6,7 @@ require_once(__DIR__.'/classes/Model.php');
  * Configuration de l'application
  */
 $app = new Eirbware\Application(array(
-    'debug' => true,
+    'debug' => (preg_match('#^/net/dev#', __DIR__)==1),
     'user.object' => true,
 ));
 
